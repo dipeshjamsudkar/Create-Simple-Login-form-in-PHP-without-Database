f49faa0a687c2ded49b11223f8ed9c4ab5534899
dipeshjamsudkar <100997927+dipeshjamsudkar@users.noreply.github.com>
2022-03-05 17:21:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $msg = "Please Enter Valid email & password";}
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
-        <title>HSC Examination 2022 - RESULT :: MAHARASHTRA STATE BOARD OF SECONDARY AND HIGHER SECONDARY EDUCATION, PUNE</title>
+        <title>Login in php without Database </title>
         
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
